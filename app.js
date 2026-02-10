03b3b00f148c020148192688051fef8e8c567612
ismailAchibat <ismailachibat4@gmail.com>
2026-02-10 10:21:52 +0100

    intégration de redis et "volumes"

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const redis = require('redis');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Bonjour depuis Docker !');
+// Créez un client Redis
+const redisClient = redis.createClient({
+   url: 'redis://redis:6379', // URL de connexion Redis
 });
 
-app.listen(port, () => {
-  console.log(`L'application écoute sur le port ${port}`);
+redisClient.on('error', err => console.log('Erreur Redis', err));
+
+app.get('/', async (req, res) => {
+   try {
+       const visits = await redisClient.incr('visits');
+       res.send(`Bonjour depuis Docker ! Vous êtes le visiteur numéro ${visits}`);
+   } catch (error) {
+       console.error(error);
+       res.status(500).send('Erreur serveur : ' + error.message);
+   }
+});
+
+app.listen(port, async () => {
+   await redisClient.connect();
+   console.log(`L'application écoute sur le port ${port}`);
 });
